1e8b457f0a0407aff482e90d95f4bec99b5f34d5
dotnetsharpist <sheyxcontacts@gmail.com>
2024-10-28 03:13:23 +0500

    Ready for Code deploy

---
+++ ProductService/Models/Product.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductApi.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string? Name { get; set; }
+
+        [MaxLength(500)]
+        public string? Description { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
