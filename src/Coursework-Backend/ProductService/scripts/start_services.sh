0782aa6458db216d5722a814fc5a5e51591a9788
dotnetsharpist <sheyxcontacts@gmail.com>
2024-10-28 03:13:01 +0500

    Ready for CodeDeploy

--- src/Coursework-Backend/ProductService/scripts/start_services.sh
+++
@@ -1,4 +0,0 @@
-# start_application.sh
-#!/bin/bash
-cd /var/www/productservice
-dotnet ProductService.dll --urls "http://localhost:5001" &
--- src/Coursework-Backend/ProductService/scripts/stop_services.sh
+++
@@ -1,4 +0,0 @@
-# stop_application.sh
-#!/bin/bash
-if pgrep -f "ProductService" > /dev/null; then
-    pkill -f "ProductService"
